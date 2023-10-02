f18fc413ac1621bc3d5ed32b5ffbb3fd79d2a973
KasenovAidarhan <kasenovaidar30@gmail.com>
2023-10-02 20:53:54 +0500

    Task0

---
+++ Tyuiu.KasenovAE.Sprint1.Task0.V5.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KasenovAE.Sprint1.Task0.V5.Lib;
+
+namespace Tyuiu.KasenovAE.Sprint1.Task0.V5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ExpressionValid()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(12, res);
+        }
+    }
+}
---
+++ Tyuiu.KasenovAE.Sprint1.Task0.V5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KasenovAE.Sprint1.Task0.V5.Lib;
+
+namespace Tyuiu.KasenovAE.Sprint1.Task0.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Написать программу, которая вычисляет выражение(1 + 2)*(1 + 9 / 3)
+            //и печатает результат на экране
+            Console.Title = "Спринт #1 | Выполнил: Касенов А. Е. | ПКТб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Касенов Айдархан Есенаевич | ПКТб-23-2                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать консольную программу, которая вычисляет выражение              *");
+            Console.WriteLine("* (1 + 2)*(1 + 9 / 3) и печатает результат на экране                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* (1 + 2)*(1 + 9 / 3)                                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            Console.WriteLine(ds.Calculate());
+            Console.ReadKey();
+        }
+    }
+}
